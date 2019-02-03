7cfa2ca0841ad132c20e3bb700abd62b334796d4
Andrewmat <andre.m.santos1@gmail.com>
2019-02-03 11:11:57 -0200

    Add hooks page

---
+++ hooks/precache-manifest.d04770640fbee4e5a340d5eafa09aef5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14b7e9d8492c774ac141",
+    "url": "/static/css/main.d2c7f7bb.chunk.css"
+  },
+  {
+    "revision": "14b7e9d8492c774ac141",
+    "url": "/static/js/main.14b7e9d8.chunk.js"
+  },
+  {
+    "revision": "5b088e8232ee72b88df7",
+    "url": "/static/js/1.5b088e82.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b92a076438051c8eafd9b243d634a4d4",
+    "url": "/index.html"
+  }
+];
