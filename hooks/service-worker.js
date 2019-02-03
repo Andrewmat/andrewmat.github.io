bdfb82aaa1ecc247589a1fe905d01b551ad8735a
Andrewmat <andre.m.santos1@gmail.com>
2019-02-03 11:15:16 -0200

    Update hooks/

--- hooks/service-worker.js
+++ hooks/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d04770640fbee4e5a340d5eafa09aef5.js"
+  "/hooks/precache-manifest.c6f3d19f0ea57ac325b0c411a07a9ad2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hooks/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
